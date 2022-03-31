5b1081cb53fa26ce934b0789020df58ffbebdd4f
ddmkim <ddmkim94@gmail.com>
2022-03-31 15:38:54 +0900

    벌크 연산

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -30,34 +30,34 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(2);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(3);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(4);
             member3.setTeam(teamB);
             em.persist(member3);
 
             Member member4 = new Member();
             member4.setUsername("회원4");
+            member4.setAge(5);
             em.persist(member4);
 
-            em.flush();
+            // 이 시점에 FLUSH 실행!(DB와 동기화, JPQL이 실행되기 때문에 FLUSH가 호출됨)
+            int result = em.createQuery("update Member m set m.age = m.age * 10").executeUpdate();
+
             em.clear();
 
-            // String query = "select m from Member m where m.id = :memberId";
-            List<Member> result = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println(member.getUsername() + ", " + member.getTeam().getName());
-            }
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
